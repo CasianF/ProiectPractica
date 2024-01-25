165748a232b393476fd25156cebf9feb9f2b9aa7
CasianF <104580929+CasianF@users.noreply.github.com>
2024-01-26 00:06:23 +0200

    Add files via upload

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,11 +2,16 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.DataFormatter;
@@ -17,78 +22,149 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class Main {
-	//lista pentru sali
-	public static List<String> sali = new ArrayList<>();
+	// lista pentru sali
+	public static List<Room> roomsList = new ArrayList<Room>();
 
-	
-	// Stiu ca nu pare mult dar mi-a luat
-	public static void main(String[] args) throws IOException {
-		File file = new File("D:\\eclipse\\Workspace\\ExcelToCsvConvertor\\MI_34_OrSt33 2.xlsx");
-		FileInputStream fis = new FileInputStream(file);
-		processSchedule(fis);
-	}
+	public static void processData() throws IOException {
+		File inputFile = new File("MI_34_OrSt33 2.xlsx");
+		FileInputStream fis = new FileInputStream(inputFile);
+		Calendar calendar = Calendar.getInstance();
+		int day = calendar.WEDNESDAY;
 
-	private static void processSchedule(FileInputStream fis) throws IOException {
 		Workbook workbook = new XSSFWorkbook(fis);
 		try {
 			Sheet sheet = workbook.getSheetAt(0);
 
-			// Ignora primele 4 coloane(anul,spec,grupa,sgr)
+			// Ignora primele 4 coloane (anul, spec, grupa, sgr)
 			int startColumn = 4;
-			// Incepe prelucrarea in functie de ziua in care esti 
-			startColumn += (1 - 1) * 7;
-			// Seteaza lungimea maxima pt ziua in care esti
-			int maxColumns = Math.min(sheet.getRow(0).getPhysicalNumberOfCells(), startColumn + 7);
-			// i = 5 pt ca sari peste primele 5 randuri
-			for (int j = startColumn; j < maxColumns; j++) {
+			startColumn = getCurrentDay(startColumn, day);
+			System.out.println(startColumn);
+			// Lungimea maxima pentru o zi
+			int maxColumns = 6 + startColumn;
 
-				System.out.println("Coloana" + (j + 1));
-				//Prelucreaza celule de pe coloane
+			// Parcurgem tabelul pe coloane si apoi pe randuri
+			for (int j = startColumn; j < maxColumns; j++) {
 				for (int i = 7; i < sheet.getPhysicalNumberOfRows(); i++) {
 					Row row = sheet.getRow(i);
 					Cell cell = row.getCell(j);
-					splitCell(cell);
-				}
-				System.out.print("Sali ocupate: ");
-				for (String a : sali) {
-					System.out.print(" " + a);
-				}
-				System.out.println();
-				//goleste lista pt ora la care ai fost
-				sali.clear();
-			}
 
+					processCell(cell, j - startColumn);
+				}
+			}
 		} finally {
 			workbook.close();
 			fis.close();
 		}
-
 	}
 
-
-	public static void splitCell(Cell cell) {
-		String cellText = cell.toString();
-		String[] arrOfStr = cellText.split(",");
-		//Exclude celulele neimportante
-		if (arrOfStr.length > 3) {
-			sali.add(arrOfStr[2]);
-		}
-
-	}
-
-	// Functie care converteste orice tip de date din excel in string
-	private static String getCellValue(Cell cell) {
-		switch (cell.getCellType()) {
-		case Cell.CELL_TYPE_STRING:
-			return cell.getStringCellValue();
-		case Cell.CELL_TYPE_NUMERIC:
-			return Double.toString(cell.getNumericCellValue());
-		case Cell.CELL_TYPE_BOOLEAN:
-			return Boolean.toString(cell.getBooleanCellValue());
-		case Cell.CELL_TYPE_BLANK:
-			return "";
+	public static int getCurrentDay(int startColumn, int day) {
+		switch (day) {
+		case Calendar.MONDAY:
+			return startColumn + 0;
+		case Calendar.TUESDAY:
+			return startColumn + 7;
+		case Calendar.WEDNESDAY:
+			return startColumn + 14;
+		case Calendar.THURSDAY:
+			return startColumn + 21;
+		case Calendar.FRIDAY:
+			return startColumn + 28;
+		case Calendar.SATURDAY:
+			return startColumn + 35;
 		default:
-			return "";
+			return startColumn;
 		}
+	}
+
+	public static void processCell(Cell cell, int time) {
+		// Get the text of the cell as a String
+		String cellText = cell.toString();
+		if (cellText.isEmpty()) {
+			return;
+		}
+
+		// Split the String into "tokens"
+		String[] tokens = cellText.split(",");
+
+		if (tokens.length < 4) {
+			return;
+		}
+
+		System.out.println("[cellText] " + cellText);
+		// Assign elements of the cell to their respective variables
+		String courseName = tokens[0];
+		char courseType = tokens[1].charAt(0);
+		String roomName = tokens[2];
+		String professorName = tokens[3];
+
+		// Check to see if the room with "roomName" exists already
+		Room roomFound = null;
+		for (Room room : roomsList) {
+			if (room.getName().equals(roomName))
+				roomFound = room;
+		}
+		// Room exists
+		if (roomFound != null) {
+			// Get the group name that corresponds to this cell
+			String groupName = getGroup(cell);
+
+			// Check to see if the room already has a TimeSlot at the time specified
+			TimeSlot timeSlotFound = roomFound.getTimeSlot(time);
+			// If it does, don't replace it
+			if (timeSlotFound != null) {
+				// Check to see if the group is not already added to the TimeSlot
+				if (!timeSlotFound.getGroups().contains(groupName))
+					// If not, add the group to the TimeSlot
+					timeSlotFound.addGroup(groupName);
+			}
+			// If the TimeSlot doesn't exist, create it with the correct attributes and add
+			// it to the room object
+			else {
+				TimeSlot timeSlot = new TimeSlot(time, courseName, courseType, professorName);
+				timeSlot.addGroup(groupName);
+				roomFound.addTimeSlot(time, timeSlot);
+			}
+
+		}
+		// Else, create the room object then add it to the list
+		else {
+			Room roomNew = new Room(roomName);
+			String groupName = getGroup(cell);
+			TimeSlot timeSlot = new TimeSlot(time, courseName, courseType, professorName);
+			timeSlot.addGroup(groupName);
+			roomNew.addTimeSlot(time, timeSlot);
+			roomsList.add(roomNew);
+		}
+	}
+
+	// Function that gets the group of a cell in the table
+	public static String getGroup(Cell cell) {
+		// Get the sheet and the row number of the cell
+		Sheet sheet = cell.getSheet();
+		int rowNumber = cell.getRowIndex();
+
+		String result = "";
+
+		// Since merged cells are un-merged when imported with Apache POI, we have to
+		// "search" for the value they hold,
+		// which is in the first cell of the merged cell (the top-left most cell)
+		// In this case, we can only ever be at or below the group text, so every
+		// iteration we check if the cell is empty,
+		// and if it is, we go up a row until we find text (The first text we find
+		// should be the group name)
+		for (int i = rowNumber; i > rowNumber - 4; i--) {
+			Row row = sheet.getRow(i);
+			Cell cellGroup = row.getCell(2);
+			if (!cellGroup.toString().isEmpty()) {
+				result = cellGroup.toString();
+				break;
+			}
+		}
+
+		return result;
+	}
+
+	public static void main(String[] args) throws IOException {
+		processData();
 	}
 }
--- src/main/java/TimeSlot.java
+++ src/main/java/TimeSlot.java
@@ -1,12 +1,21 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class TimeSlot {
 
-    private int time;
+	private int time;
     private String courseName;
     private char courseType;
     private String professorName;
     private List<String> groupNames;
+
+    public TimeSlot(int time, String courseName, char courseType, String professorName) {
+        this.time = time;
+        this.courseName = courseName;
+        this.courseType = courseType;
+        this.professorName = professorName;
+        this.groupNames = new ArrayList<String>();
+    }
 
     public TimeSlot(int time, String courseName, char courseType, String professorName, List<String> groupNames) {
         this.time = time;
@@ -58,11 +67,18 @@ public class TimeSlot {
         return result;
     }
 
+    public List<String> getGroups() {
+        return this.groupNames;
+    }
+
     public void setGroupNames(List<String> groupNames) {
         this.groupNames = groupNames;
     }
 
-    
+    public void addGroup(String groupName)
+    {
+        this.groupNames.add(groupName);
+    }
 
     public char getCourseType() {
         return courseType;
@@ -76,7 +92,5 @@ public class TimeSlot {
     public String toString() {
         return courseName + ", " + professorName + "\n" + groupNames;
     }
-
-	
 
 }
